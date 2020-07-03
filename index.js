4d084e430c9abc8cd06367420ad7d10e7a7ad909
romakv4 <romakv4@gmail.com>
2020-07-03 18:01:26 +0600

    Add small instrumentation for app

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// подключение express
 const express = require("express");
 const bodyParser = require('body-parser');
-// создаем объект приложения
+
 const app = express();
-// Подключаем body-parser
 app.use(bodyParser.json());
 
-// Определяем обработчик корневого урла с методом POST
 app.post("/", function(request, response) {
-    const { firstArg, secondArg, operation } = request.body; // деструктуризация
+    const { firstArg, secondArg, operation } = request.body;
     if (operation === '+') {
         response.status(200).json(firstArg + secondArg);
     } else if (operation === '-') {
@@ -19,5 +16,7 @@ app.post("/", function(request, response) {
         response.status(200).json(firstArg * secondArg);
     }
 });
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log('App started on https://localhost:3000');
+});
