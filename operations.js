9cb1d287ff41987224816ffdb80fb217a7ce626c
romakv4 <romakv4@gmail.com>
2020-07-04 22:35:08 +0600

    Add simple error handling

--- operations.js
+++ operations.js
@@ -4,11 +4,23 @@ const subtraction = (minuend, subtrahend) => minuend - subtrahend;
 
 const multiplication = (multiplicand, multiplier) => multiplicand * multiplier;
 
-const division = (dividend, divider) => dividend / divider;
+const division = (dividend, divider) => {
+    if (divider !== 0) {
+        return dividend / divider;
+    } else {
+        throw new Error('Divider can\'t be zero');
+    }
+};
 
 const exponentiation = (basis, power) => Math.pow(basis, power);
 
-const percent = (basis, percent) => basis * (percent / 100);
+const percent = (basis, percent) => {
+    if (percent >= 0) {
+        return basis * (percent / 100);
+    } else {
+        throw new Error('Percent can\'t be negative');
+    }
+};
 
 const twoArgsOperations = {
     "+": addition,
