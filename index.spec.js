5bd292c9cbb396b7d0f35ee504cfba60f7134881
romakv4 <romakv4@gmail.com>
2020-07-05 22:18:21 +0600

    Add jest and supertest example

---
+++ index.spec.js
@@ -0,0 +1,22 @@
+const app = require('./index');
+const supertest = require('supertest');
+const request = supertest(app);
+
+describe('API', () => {
+    it('Sum request', () => {
+        return request
+            .post('/two-args')
+            .send(
+                {
+                    firstArg: 1,
+                    secondArg: 2,
+                    operation: "+"
+                }
+            )
+            .expect('Content-type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body).toBe(3);
+            });
+    });
+});
